e891b13c14d560d1d5d897aaed3e0b87035e9846
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-04-20 00:48:42 +0500

    Deleted the surplus file

--- fact.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-fact = requests.get("https://uselessfacts.jsph.pl/random.json?language=en").json()["text"]
-
-with open("FACT.md", "w") as f:
-    f.write(f"## 🧠 Fact of the Day\n\n> {fact}\n")
